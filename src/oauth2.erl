dd5c275116bb192768979bb407a31f3b66c952a9
casper <casper.zx@gmail.com>
2013-08-23 00:06:55 +0400

    Simplified usage of oauth2.

--- src/oauth2.erl
+++ src/oauth2.erl
@@ -9,10 +9,8 @@
 
 -export([start/0, stop/0]).
 
--export([complete/2]).
-
 -export([auth_url/2, auth_url/3, auth_result/1, auth_state/1]).
--export([request_token/3, refresh_token/3]).
+-export([request_token/3, refresh_token/2]).
 
 -include("oauth2.hrl").
 -include("oauth2_config.hrl").
@@ -22,11 +20,9 @@
 start() -> application:start(?MODULE).
 stop() -> application:stop(?MODULE).
 
-complete(Token, Client) -> #oauth2{token = Token, client = Client}.
-
-auth_url(Network, Client) -> auth_url(Network, Client, []).
-auth_url(Network, Client, State) -> utils_http:url(?AuthUri(Network),
-	?AuthUrlOptions(Network, Client, State), encode_value).
+auth_url(Network, OAuth) -> auth_url(Network, OAuth, []).
+auth_url(Network, OAuth, State) -> utils_http:url(?AuthUri(Network),
+	?AuthUrlOptions(Network, OAuth, State), encode_value).
 
 auth_state(Result) -> utils_lists:keyfind("state", Result).
 
@@ -35,22 +31,16 @@ auth_result(Result) -> auth_result(Result,
 auth_result(Result, false) -> utils_lists:keyfind("code", Result);
 auth_result(_Result, Error) -> {error, Error}.
 
-request_token(Network, Client, Code) ->
-	token(request, Network, Client, Code).
+request_token(Network, OAuth, Code) ->
+	set_token(token(request, Network, OAuth, Code), OAuth).
 
-refresh_token(Network, Client, Token) ->
-	case token(refresh, Network, Client, Token) of
-		{ok, T = #oauth2_token{refresh = undefined}} ->
-			{ok, T#oauth2_token{refresh = Token#oauth2_token.refresh}};
-		Other -> Other
-	end.
+refresh_token(Network, OAuth) -> set_token(restore_refresh_token(
+	OAuth, token(refresh, Network, OAuth, [])), OAuth).
 
-token(Mode, Network, Client, Grant) ->
-	read_token(Network, httpc:request(post, {
-		?AccessTokenUri(Network), [], ?ContentType,
-		utils_http:query_string(?AccessTokenOptions(
-			Mode, Network, Client, Grant), encode_value)
-	}, [], [])).
+token(Mode, Network, OAuth, Code) -> read_token(Network, httpc:request(post, {
+	?AccessTokenUri(Network), [], ?ContentType, utils_http:query_string(
+		?AccessTokenOptions(Mode, Network, OAuth, Code), encode_value)
+}, [], [])).
 
 read_token(Network, {ok, {{_, 200, _}, _Headers, Body}})
 	when Network == live; Network == google
@@ -70,6 +60,13 @@ read_token(Network, {ok, {{_, _, _}, _Headers, Body}}) ->
 
 read_error(facebook, [{<<"error">>, Error}]) -> {error, Error};
 read_error(_, Error) -> {error, Error}.
+
+restore_refresh_token(OAuth, {ok, T = #oauth2_token{refresh = undefined}}) ->
+	{ok, T#oauth2_token{refresh = OAuth#oauth2.token#oauth2_token.refresh}};
+restore_refresh_token(_OAuth, Other) -> Other.
+
+set_token({ok, Token}, OAuth) -> {ok, OAuth#oauth2{token = Token}};
+set_token(Error, _OAuth) -> Error.
 
 btl(Binary) -> binary_to_list(Binary).
 ltb(List) -> list_to_binary(List).
